a370c3091b769df569a62dbfd355ea7331a96f7d
Anton <antybumer@gmail.com>
2022-12-03 00:36:18 +0300

    final fixs

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import Header from './components/Header.jsx'
+import Header from './components/header'
 import Footer from './components/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
